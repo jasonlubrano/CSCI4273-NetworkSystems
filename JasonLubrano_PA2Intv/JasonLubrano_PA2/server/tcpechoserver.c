d02ab8f3b857f9fcc7032c1b7b53778a9977e177
jasonlubrano <jalubrano1@gmail.com>
2019-11-14 19:25:17 -0700

    started PA3

---
+++ JasonLubrano_PA2Intv/JasonLubrano_PA2/server/tcpechoserver.c
@@ -0,0 +1,293 @@
+/* 
+ * tcpechoserver.c - A concurrent TCP echo server using threads
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>      /* for fgets */
+#include <strings.h>     /* for bzero, bcopy */
+#include <unistd.h>      /* for read, write */
+#include <sys/socket.h>  /* for socket use */
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <dirent.h>
+
+#define MAXLINE  8192  /* max text line length */
+#define SHORTBUF  256  /* max text line length */
+#define MAXBUF   8192  /* max I/O buffer size */
+#define LISTENQ  1024  /* second argument to listen() */
+
+#define MSGERRR "\x1b[31m"
+#define MSGSUCC "\x1b[32m"
+#define MSGNORM "\x1b[0m"
+#define MSGTERM "\x1b[35m"
+#define MSGWARN "\x1b[33m"
+
+int open_listenfd(int port);
+void echo(int connfd);
+void *thread(void *vargp);
+int is_valid_URL(const char* urlarg);
+int is_valid_VER(const char* verarg);
+const char *get_fname_ext(const char *fname);
+
+int main(int argc, char **argv) 
+{
+    int listenfd, *connfdp, port, clientlen=sizeof(struct sockaddr_in);
+    struct sockaddr_in clientaddr;
+    pthread_t tid; 
+
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(0);
+    }
+    port = atoi(argv[1]);
+
+    listenfd = open_listenfd(port);
+    while (1) {
+        connfdp = malloc(sizeof(int));
+        *connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
+        pthread_create(&tid, NULL, thread, connfdp);
+    }
+}
+
+/* thread routine */
+void * thread(void * vargp) 
+{  
+    int connfd = *((int *)vargp);
+    pthread_detach(pthread_self()); 
+    free(vargp);
+    echo(connfd);
+    close(connfd);
+    return NULL;
+}
+
+/*
+ * echo - read and echo text lines until client closes connection
+ */
+void echo(int connfd) 
+{
+    size_t n;
+    char buf[MAXLINE];
+    char metbuf[SHORTBUF];
+    char urlbuf[MAXLINE];
+    char doturlbuf[MAXLINE];
+    char verbuf[SHORTBUF];
+    char ftype[SHORTBUF];
+    char contenttype[SHORTBUF];
+    char tempstr[MAXLINE];
+    char *dot = ".";
+    char httpmsggiven[]="HTTP/1.1 200 Document Follows\r\nContent-Type:text/html\r\nContent-Length:32\r\n\r\n<html><h1>Hello CSCI4273 Course!</h1>";
+    int is_error500 = 0;
+    char error500msg[]="HTTP/1.1 500 Internal Server Error\r\nContent-Type:text/plain\r\nContent-Length:0\r\n\r\n";
+    int is_validurl = 0;
+    int is_validver = 0;
+
+    bzero(buf, MAXLINE);
+    bzero(metbuf, SHORTBUF);
+    bzero(urlbuf, MAXLINE);
+    bzero(doturlbuf, MAXLINE);
+    bzero(verbuf, SHORTBUF);
+    bzero(ftype, SHORTBUF);
+    bzero(contenttype, SHORTBUF);
+    bzero(tempstr, MAXLINE);
+    
+    n = read(connfd, buf, MAXLINE);
+    printf(MSGSUCC "server received the following request:\n%s" MSGSUCC "\n", buf);
+    
+    printf(MSGWARN "PARSING" MSGNORM "\n");
+    char* token1 = strtok(buf, " ");
+    size_t tk1len = strlen(token1);
+    strncpy(metbuf, token1, tk1len);
+
+    char* token2 = strtok(NULL, " ");
+    size_t tk2len = strlen(token2); 
+    strncpy(urlbuf, token2, tk2len);
+
+    char* token3 = strtok(NULL, "\r\n");
+    size_t tk3len = strlen(token3);
+    strncpy(verbuf, token3, tk3len);
+    
+    bzero(buf, MAXLINE);
+    
+    printf(MSGSUCC "GETTING FILE" MSGNORM "\n");
+    
+    FILE *fp = NULL;
+
+    if (is_valid_VER(verbuf)){
+        is_validver = 1;
+    } else {
+        is_validver = 0;
+        is_error500 = 1;
+    }
+
+    if (is_valid_URL(urlbuf)){
+        is_validurl = 1;
+    } else {
+        is_validurl = 0;
+        is_error500 = 1;
+    }
+
+    if(is_validurl && is_validver){
+        printf(MSGTERM "VALID URL AND VERSION" MSGNORM "\n");
+        strcat(doturlbuf, dot);
+        strcat(doturlbuf, urlbuf);
+        printf(MSGTERM "doturlbuf: %s" MSGTERM "\n", doturlbuf);
+        if(!strcmp(doturlbuf, "./")){
+            printf(MSGTERM "DEFAULT WEBPAGE" MSGNORM "\n");
+            fp = fopen("index.html", "rb");
+            printf(MSGSUCC "READING FILE" MSGNORM "\n");
+            fseek(fp, 0L, SEEK_END);
+            n = ftell(fp);
+            rewind(fp);
+            printf(MSGSUCC "FILE READ" MSGNORM "\n");
+            strcpy(ftype, "html");
+            printf(MSGTERM "ftype: %s" MSGNORM "\n", ftype);
+        } else if(fp = fopen(doturlbuf, "rb")) {
+            printf(MSGSUCC "READING FILE" MSGNORM "\n");
+            fseek(fp, 0L, SEEK_END);
+            n = ftell(fp);
+            rewind(fp);
+            printf(MSGSUCC "FILE READ" MSGNORM "\n");
+            strcpy(ftype, get_fname_ext(urlbuf));
+            printf(MSGTERM "ftype: %s" MSGNORM "\n", ftype);
+        } else {
+            printf(MSGERRR "FILE DOES NOT EXIST" MSGNORM "\n");
+            is_error500 = 1;
+        }
+        
+        if(is_error500 == 0){
+            if(!strcmp(ftype, "html")){
+                /* set new ftype to text/html */
+                strcpy(contenttype, "text/html");
+            } else if (!strcmp(ftype, "txt")){
+                /* set new ftype to text/plain */
+                printf(MSGTERM "TXT FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "text/plain");
+            } else if (!strcmp(ftype, "png")){
+                /* set new ftype to image/png */
+                printf(MSGTERM "PNG FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "image/png");
+            } else if (!strcmp(ftype, "gif")){
+                /* set new ftype to image/gif */
+                printf(MSGTERM "GIF FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "image/gif");
+            } else if (!strcmp(ftype, "jpg")){
+                /* set new ftype to image/jpg */
+                printf(MSGTERM "JPG FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "image/jpg");
+            } else if (!strcmp(ftype, "css")){
+                /* set new ftype to text/css */
+                printf(MSGTERM "CSS FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "text/css");
+            } else if (!strcmp(ftype, "js")){
+                /* set new ftype to application/java */
+                printf(MSGTERM "JS FILETYPE" MSGNORM "\n");
+                strcpy(contenttype, "application/java");
+            } else {
+                printf(MSGERRR "NOT A VALID FILETYPE" MSGNORM "\n");
+            }
+            printf(MSGTERM "content type: %s" MSGNORM "\n", contenttype);
+
+            char *filebuff = malloc(n);
+            fread(filebuff, 1, n, fp);
+            
+            char tempbuff[MAXLINE];
+            sprintf(tempbuff, "HTTP/1.1 200 Document Follows\r\nContent-Type:%s\r\nContent-Length:%ld\r\n\r\n", contenttype, n);
+            
+            char *httpmsg = malloc(n + strlen(tempbuff));
+            printf(MSGTERM "httpmsg: %s" MSGNORM "\n", httpmsg);
+            sprintf(httpmsg, "%s", tempbuff);
+            memcpy(httpmsg + strlen(tempbuff), filebuff, n);
+            n += strlen(tempbuff);
+
+            //printf(MSGTERM"server returning a http message with the following content.\n%s" MSGNORM "\n", httpmsg);
+            write(connfd, httpmsg, n);
+        } else {
+            /* file doesnt exist or something, have server ignore */
+            printf(MSGERRR "SENDING ERROR MESSAGE" MSGNORM "\n");
+            n = strlen(error500msg);
+            write(connfd, error500msg, n);
+        }
+    } else {
+        printf(MSGERRR "NOT VALID" MSGNORM "\n");
+        printf(MSGERRR "SENDING ERROR MESSAGE" MSGNORM "\n");
+        n = strlen(error500msg);
+        write(connfd, error500msg, n);
+    }
+    
+    bzero(buf, MAXLINE);
+    bzero(metbuf, SHORTBUF);
+    bzero(urlbuf, MAXLINE);
+    bzero(doturlbuf, MAXLINE);
+    bzero(verbuf, SHORTBUF);
+    bzero(ftype, SHORTBUF);
+    bzero(contenttype, SHORTBUF);
+    bzero(tempstr, MAXLINE);
+}
+
+const char *get_fname_ext(const char *fname) {
+    const char *period = strrchr(fname, '.');
+    if(!period || period == fname){
+        return "";
+    }
+    return period + 1;
+}
+
+int is_valid_URL(const char* urlarg){
+    int len_urlarg = strlen(urlarg);
+    if ((urlarg != NULL) && (urlarg[0] == '\0')) {
+        printf("urlarg is empty\n");
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int is_valid_VER(const char* verarg){
+    if(strlen(verarg) == 0) return 0;
+    if ((verarg != NULL) && (verarg[0] == '\0')) {
+        printf("verarg is empty\n");
+        return 0;
+    } else if(strcmp(verarg, "HTTP/1.1") == 0) {
+        return 1;
+    } else if(strcmp(verarg, "HTTP/1.0") == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* 
+ * open_listenfd - open and return a listening socket on port
+ * Returns -1 in case of failure 
+ */
+int open_listenfd(int port) 
+{
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+  
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        return -1;
+
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+                   (const void *)&optval , sizeof(int)) < 0)
+        return -1;
+
+    /* listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0)
+        return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
+        return -1;
+    return listenfd;
+} /* end open_listenfd */
---
+++ JasonLubrano_PA3/client/clientproxy.c
@@ -0,0 +1,328 @@
+/*
+ * Jason Lubrano
+ * CSCI4273 - Network Systems - Dr. Sangtae Ha
+ * proxy_client.c
+ * $ ./client <port>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <arpa/inet.h>
+#include <ctype.h>
+
+#define BUFFSIZE 1024
+#define MSGERRR "\x1b[31m"
+#define MSGSUCC "\x1b[32m"
+#define MSGNORM "\x1b[0m"
+#define MSGTERM "\x1b[35m"
+#define MSGWARN "\x1b[33m"
+
+#define TRUE 1
+
+// error message
+void error(char *msg){
+    perror(msg);
+    exit(0);
+}
+
+void errorcheckrecv(int nn){
+    if(nn < 0){
+        error("ERROR in recvfrom");
+    }
+}
+
+void errorchecksend(int nn){
+    if(nn < 0){
+        error("ERROR in sendto");
+    }
+}
+
+void serverecho(char* buff){
+    printf(MSGTERM "%s" MSGNORM "\n", buff);
+}
+
+// handles geting input
+void buffinput(char* buffcmd){
+    // get input from user
+    char buf[BUFFSIZE];
+    bzero(buf, BUFFSIZE);
+    printf(MSGTERM "Enter command" MSGNORM "\n");
+    printf("cmd> ");
+    fgets(buf, BUFFSIZE, stdin);
+    int i=0;
+    for(i=0; i<=strlen(buf); i++){
+        buffcmd[i] = buf[i];
+    }
+    buffcmd[strlen(buffcmd)-1] = '\0';
+}
+
+void buffparse(char *buf, char *argcmd, char *argfile){
+    int i=0;
+    char delim[] = " ";
+    
+    memset(argcmd, 0, BUFFSIZE * (sizeof(char)));
+    memset(argfile, 0, BUFFSIZE * (sizeof(char)));
+
+    char* token1 = strtok(buf, delim);
+    size_t tk1len = strlen(token1);
+    strncpy(argcmd, token1, tk1len);
+
+    if(strcmp(argcmd, "ls") == 0){
+        argfile = "";
+    } else if (strcmp(argcmd, "exit") == 0) {
+        argfile = "";
+    } else if ((strcmp(argcmd, "put") == 0) 
+            || (strcmp(argcmd, "get") == 0)
+            || (strcmp(argcmd, "delete") == 0)){
+        char* token2 = strtok(NULL, delim);
+        size_t tk2len = strlen(token2);
+        strncpy(argfile, token2, tk2len);
+    } else {
+        argcmd=""; argfile="";
+    }
+}
+
+int main(int argc, char **argv){
+    // check command line argt
+    if(argc != 3){
+        printf(MSGERRR "Wrong number of arguments." MSGNORM "\n");
+        printf(MSGERRR "Usage: %s <port>" MSGNORM "\n", argv[0]);
+        exit(0);
+    }
+
+    // set hostname
+    char *hostname = argv[1];
+    printf(MSGNORM "Hostname set: " MSGNORM "\t");
+    printf(MSGSUCC "%s" MSGNORM "\n", hostname);
+
+    // set port
+    int portno = atoi(argv[2]);
+    printf(MSGNORM "Port set: " MSGNORM "\t");
+    printf(MSGSUCC "%d" MSGNORM "\n", portno);
+
+    // create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){
+        error(MSGERRR "ERROR opening Socket" MSGNORM "\n");
+    }
+
+    // get host by name = get servers dns entry
+    struct hostent *server = gethostbyname(hostname);
+    if(server == NULL){
+        printf(MSGERRR "ERROR, no such host: %s" MSGNORM "\n", hostname);
+        exit(0);
+    }
+
+    // server address build
+    struct sockaddr_in serveraddr;
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char*)server->h_addr,
+        (char*)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(portno);
+
+    // basic messages for debugging packets
+    char ready[BUFFSIZE] = "Ready to connect";
+    char acknoledge[BUFFSIZE] = "Message received";
+    char ack[BUFFSIZE];
+
+    // server setup
+    int serverlen = sizeof(serveraddr);
+    int n = sendto(sockfd, ready, sizeof(ready), MSG_WAITALL,
+                (const struct sockaddr *) &serveraddr, serverlen);
+    if(n < 0){
+        error(MSGERRR "ERROR in connect\n" MSGNORM);
+    }
+
+    char buff[BUFFSIZE];
+    n = recvfrom(sockfd, buff, BUFFSIZE, 0, (struct sockaddr *) &serveraddr, &serverlen);
+    if (n < 0){
+      error("ERROR in recvfrom");
+    }
+    printf("Echo from server: %s, n = %d \n", buff, n);
+
+    // get input from user
+    // get cmd and filename
+    char argb[BUFFSIZE]; //command
+    char argf[BUFFSIZE]; //filename
+    char endfio[BUFFSIZE] = "endfio";
+
+    FILE *fp;
+
+    // handle input from user
+    buffinput(buff);
+    printf(MSGNORM "Input: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, buff);
+    printf(MSGNORM " -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(buff));
+
+    buffparse(buff, argb, argf);
+    printf(MSGNORM "Command: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, argb);
+    printf(MSGNORM "  -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(argb));
+
+    printf(MSGNORM "File: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, argf);
+    printf(MSGNORM "  -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(argf));
+
+    do{        
+        n = sendto(sockfd, argb, sizeof(argb), MSG_WAITALL,
+                    (const struct sockaddr *) &serveraddr, serverlen);
+        errorchecksend(n);
+        
+        n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                    (struct sockaddr *) &serveraddr, &serverlen);
+        errorcheckrecv(n);
+        serverecho(buff);
+        
+        n = sendto(sockfd, argf, sizeof(argf), MSG_WAITALL,
+                    (const struct sockaddr *) &serveraddr, serverlen);
+        errorchecksend(n);
+        
+        n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                    (struct sockaddr *) &serveraddr, &serverlen);
+        errorcheckrecv(n);
+        serverecho(buff);
+
+        // client handle cmds
+        /***********************************/
+        if (strcmp(argb, "put") == 0){
+            FILE *fp = fopen(argf, "r");
+            size_t nbytes = 0;
+            int packetn;
+            memset(buff, 0, BUFFSIZE * (sizeof(char)));
+            memset(ack, 0, BUFFSIZE * (sizeof(char)));
+            while((nbytes = fread(buff+1, 1, BUFFSIZE-1, fp)) > 0){
+                buff[0] = 'p';
+                n = sendto(sockfd, buff, nbytes+1, 0,
+                        (struct sockaddr*)&serveraddr, serverlen);
+                errorchecksend(n);
+                printf(MSGWARN "Packet %d Sent" MSGNORM " Size %ld\n", ++packetn, nbytes);
+                n = recvfrom(sockfd, ack, BUFFSIZE, 0,
+                        (struct sockaddr *)&serveraddr, &serverlen);
+                errorcheckrecv(n);
+                serverecho(ack);
+                if(ack[0] == 'x'){
+                    printf(MSGWARN "Server did not receive packet %d" MSGNORM "\n", packetn);
+                    n = sendto(sockfd, buff, nbytes+1, 0,
+                            (struct sockaddr*)&serveraddr, serverlen);
+                }
+            }
+            printf(MSGSUCC "File Sent" MSGNORM "\n");
+        /***********************************/
+        } else if (strcmp(argb, "get") == 0){
+            printf(MSGTERM "Client fetching file:" MSGNORM " %s \n", argf);
+            /* message handling */
+            while(TRUE){
+                memset(buff, 0, BUFFSIZE * (sizeof(char)));
+                n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                        (struct sockaddr *)&serveraddr, &serverlen);
+                errorcheckrecv(n);
+                if(buff[0]='g'){
+                    fp = fopen(argf, "a");
+                    fwrite(buff+1, 1, n-1, fp);
+                    fflush(fp);
+                    fclose(fp);
+                    if(n!=BUFFSIZE){
+                        n = sendto(sockfd, acknoledge, sizeof(acknoledge), MSG_WAITALL, 
+                                (struct sockaddr *) &serveraddr, serverlen);
+                        errorchecksend(n);
+                        printf(MSGSUCC "File saved" MSGNORM "\n");
+                        break;
+                    }
+                    n = sendto(sockfd, acknoledge, sizeof(acknoledge), MSG_WAITALL, 
+                            (struct sockaddr *) &serveraddr, serverlen);
+                    errorchecksend(n);
+                } else {
+                    // BONUS: we missed the packet or corrupted
+                    // we have to resend that packet
+                    buff[0] == 'x';
+                    n = sendto(sockfd, buff, BUFFSIZE, 0,
+                        (struct sockaddr*)&serveraddr, serverlen);
+                    errorchecksend(n);
+                }
+            }
+        /***********************************/
+        } else if (strcmp(argb, "ls") == 0){
+            printf(MSGTERM "Listing files on server" MSGNORM "\n");
+            /* handle file listing */
+            memset(buff, 0, BUFFSIZE * (sizeof(char)));
+            n=256; // size always sent from strdirent->d_name
+            while (n != 8){
+                n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                            (struct sockaddr *) &serveraddr, &serverlen);
+                if(strcmp(buff, "endlist") != 0){
+                    printf(">> %s\n", buff);
+                }
+                memset(buff, 0, BUFFSIZE * (sizeof(char)));
+            }
+            memset(buff, 0, BUFFSIZE * (sizeof(char)));
+        /***********************************/
+        } else if (strcmp(argb, "delete") == 0){
+            /* handle file deletion */
+            printf(MSGWARN "Deleting file:" MSGNORM " %s \n", argf);
+            n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                        (struct sockaddr *) &serveraddr, &serverlen);
+            printf(MSGTERM "%s" MSGNORM "\n", buff);
+            memset(buff, 0, BUFFSIZE * (sizeof(char)));
+        /***********************************/
+        } else {
+            // handle getting input again
+            printf(MSGERRR "NOT VALID INPUT" MSGNORM "\n");
+            printf(MSGWARN "Commands: put <filename>, get <filename>, ls, exit" MSGNORM "\n");
+        }
+    memset(&buff, 0, BUFFSIZE * (sizeof(char)));
+    memset(&argf, 0, BUFFSIZE * (sizeof(char)));
+    memset(&argb, 0, BUFFSIZE * (sizeof(char)));
+
+    buffinput(buff);
+    printf(MSGNORM "Input: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, buff);
+    printf(MSGNORM " -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(buff));
+
+    buffparse(buff, argb, argf);
+    printf(MSGNORM "Command: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, argb);
+    printf(MSGNORM "  -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(argb));
+
+    printf(MSGNORM "File: " MSGNORM);
+    printf(MSGSUCC "%s" MSGNORM, argf);
+    printf(MSGNORM "  -- Size: " MSGNORM);
+    printf(MSGWARN "%ld" MSGNORM "\n", strlen(argf));
+
+    }while(strcmp(argb, "exit") != 0);
+
+    /* exit handling */
+    n = sendto(sockfd, argb, sizeof(argb), MSG_WAITALL,
+            (const struct sockaddr *) &serveraddr, serverlen);
+    errorchecksend(n);
+    
+    n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                (struct sockaddr *) &serveraddr, &serverlen);
+    errorcheckrecv(n);
+    serverecho(buff);
+    
+    n = sendto(sockfd, argf, sizeof(argf), MSG_WAITALL,
+                (const struct sockaddr *) &serveraddr, serverlen);
+    errorchecksend(n);
+    
+    n = recvfrom(sockfd, buff, BUFFSIZE, 0,
+                (struct sockaddr *) &serveraddr, &serverlen);
+    errorcheckrecv(n);
+    serverecho(buff);
+
+    close(sockfd);
+    return 0;
+}
