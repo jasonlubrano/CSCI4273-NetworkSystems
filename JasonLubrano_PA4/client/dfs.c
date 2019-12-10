263d23cc325a961060be9ecfc756470b8e243c0c
jasonlubrano <jalubrano1@gmail.com>
2019-12-10 00:47:46 -0700

    Client: GET, PUT, CONN. Server: LIST, GET, PUT

--- JasonLubrano_PA4/client/dfs.c
+++ JasonLubrano_PA4/client/dfs.c
@@ -1,7 +1,7 @@
 /* 
  * Jason Lubrano
  * CSCI4273 - Ha - PA4
- * client/dfs.c - client sided distributed file sharing system
+ * client/dfs.c - client side of the distributed file sharing system
  */
 
 
@@ -46,10 +46,15 @@
 /*
  * lengths
  */
-#define MD5_LEN     16      /* max length for md5 */
+#define NUMSERVS    4       /* max number of servers */
+#define SERVPORT    8       /* max server port length */
+#define SERVNAME    16      /* max server name */
+#define FILENAME    32      /* max size of file name*/  
+#define SERVADDR    64      /* max server addr */
+#define FILEPRTS    64      /* max num of file parts */
 #define MAXLINE     256     /* max text line length */
-#define MAXBUFF     8192    /* max text line length */
 #define LISTENQ     1024    /* second argument to listen() */
+#define MAXBUFF     8192    /* max text line length */
 
 
 /*
@@ -65,6 +70,12 @@ void welcome_message();
 void closing_message();
 void ret_exit();
 void data_from_config(const char* configFile);
+void lsh_loop();
+int handle_LIST();
+int handle_GET(const char* filename);
+int handle_PUT(const char* filename);
+int handle_EXIT();
+int send_command_to_server(int serverNumber, char* argcmd, ...);
 
 
 /*
@@ -74,9 +85,9 @@ struct timeval timeout;
 
 
 struct ServerConfigs {
-	char	ServerNames[4][16];
-	char	ServerAddresses[4][64];
-	char	ServerPorts[4][8];
+	char	ServerNames[NUMSERVS][SERVNAME];
+	char	ServerAddresses[NUMSERVS][SERVADDR];
+	char	ServerPorts[NUMSERVS][SERVPORT];
 	int		ServerNum;
 	char	ServerUsername[MAXLINE];
 	char	ServerPassword[MAXLINE];
@@ -103,10 +114,14 @@ int main(int argc, char **argv){
     }
     
     data_from_config(configFile);
+    
+    lsh_loop();
 
     closing_message();
+
     return 0;
 }
+
 
 /*
  * function definitons
@@ -138,7 +153,7 @@ void ret_exit(){
 
 /* gets data from config file */
 void data_from_config(const char* configFile){
-    printf(MSGNORM "--Getting Data from Config--" MSGNORM "\n");
+    if(VERBOSE) { printf(MSGNORM "--Getting Data from Config--" MSGNORM "\n"); }
     FILE* file = fopen(configFile, "r");
     if(file == NULL){ printf(MSGERRR ".config file is NULL \nDoes it exist?" MSGNORM "\n"); ret_exit(); }
     else{
@@ -153,7 +168,7 @@ void data_from_config(const char* configFile){
         char* username;
         char* password;
         char* delim = " ";
-        int i = 0;
+        int servercount = 0;
         if(VERBOSE){ printf(".Reading through file line by line \n"); }
         while (fgets(line, sizeof(line), file))
         {
@@ -165,6 +180,7 @@ void data_from_config(const char* configFile){
 
             if(strcmp("Server", firstinfo) == 0) {
                 if(VERBOSE){ printf(MSGSUCC "Match on server" MSGNORM "\n"); }
+                
                 servername = strtok(NULL, delim);
                 if(VERBOSE){ printf("servername: %s \n", servername); }
                 serverIP = strtok(NULL, ":");
@@ -172,41 +188,186 @@ void data_from_config(const char* configFile){
                 serverPort = strtok(NULL, ":");
                 if(VERBOSE){ printf("serverPort: %s \n", serverPort); }
                 
-                strcpy(serverconfigs.ServerNames[i], servername);
-                strcpy(serverconfigs.ServerAddresses[i], serverIP);
-                strcpy(serverconfigs.ServerPorts[i], serverPort);
-
-                printf("Server Config Name: %s \n", serverconfigs.ServerNames[i]);
-                printf("Server Config Address: %s \n", serverconfigs.ServerAddresses[i]);
-                printf("Server Config Port: %s \n", serverconfigs.ServerPorts[i]);
+                strcpy(serverconfigs.ServerNames[servercount], servername);
+                strcpy(serverconfigs.ServerAddresses[servercount], serverIP);
+                strcpy(serverconfigs.ServerPorts[servercount], serverPort);
+-
+                printf("Server Config -> Server Name: %s \n", serverconfigs.ServerNames[servercount]);
+                printf("Server Config -> Server Address: %s \n", serverconfigs.ServerAddresses[servercount]);
+                printf("Server Config -> Server Port: %s \n", serverconfigs.ServerPorts[servercount]);
 
             } else if(strcmp("Username:", firstinfo) == 0){
                 if(VERBOSE){ printf(MSGSUCC "Match on Username" MSGNORM "\n"); }
+                
                 username = strtok(NULL, delim);
                 if(VERBOSE){ printf("username: %s \n", username); }
                 
                 strcpy(serverconfigs.ServerUsername, username);
-                printf("Server Config Username: %s \n", serverconfigs.ServerUsername);
+                printf("Server Config -> Username: %s \n", serverconfigs.ServerUsername);
 
             } else if(strcmp("Password:", firstinfo) == 0){
                 if(VERBOSE){ printf(MSGSUCC "Match on Password" MSGNORM "\n"); }
+                
                 password = strtok(NULL, delim);
                 if(VERBOSE){ printf("password: %s \n", password); }
                 
                 strcpy(serverconfigs.ServerPassword, password);
-                printf("Server Config Password: %s \n", serverconfigs.ServerPassword);
+                printf("Server Config -> Password: %s \n", serverconfigs.ServerPassword);
 
             } else {
                 printf(MSGERRR "ERROR IN CONFIG PARSING" MSGNORM "\n\n");
                 ret_exit();
             }
 
-            ++i;
+            ++servercount;
         }
-        serverconfigs.ServerNum = i-2; // minus 2 bc username / password
-        printf("Server Config Server Count: %d \n", serverconfigs.ServerNum);
+        serverconfigs.ServerNum = servercount-2; // minus 2 bc username / password
+        printf("Server Config -> Server Count: %d \n", serverconfigs.ServerNum);
 
         fclose(file);
         
     }
+}
+
+
+/* runs the program through the shell, inits servers */
+void lsh_loop(){
+    if(VERBOSE) { printf(MSGNORM "--Running Shell Loop--" MSGNORM "\n"); }
+    char *cmd = NULL;
+    ssize_t cmdlength = 0;
+    ssize_t inputcmdlength;
+    char command[8], args[64];
+    int is_running = 1;
+
+    while(is_running){
+        printf("%s@DFS_Client> ", serverconfigs.ServerUsername);
+        cmdlength = getline(&cmd, &inputcmdlength, stdin);
+        cmd[cmdlength-1] = '\0';
+        if(VERBOSE) { printf("    input command: %s \n", cmd); }
+        
+        if(strcmp(cmd, "LIST") == 0){
+            handle_LIST();
+
+        } else if(strncmp(cmd, "GET", 3) == 0) {
+            char* GETtok = strtok(cmd, " ");
+            char* fname = strtok(NULL, " ");
+            if(VERBOSE){ printf("Filename: %s\n", fname); }
+            if(fname == NULL){ printf(MSGERRR "No filename specified" MSGNORM "\n");}
+            else{ handle_GET(fname); }
+
+        } else if(strncmp(cmd, "PUT", 3) == 0) {
+            char* PUTtok = strtok(cmd, " ");
+            char* fname = strtok(NULL, " ");
+            if(VERBOSE){ printf("Filename: %s\n", fname); }
+            if(fname == NULL){ printf(MSGERRR "No filename specified" MSGNORM "\n");}
+            else { handle_PUT(fname); }
+
+        } else if(strcmp(cmd, "EXIT") == 0) {
+            is_running = handle_EXIT();
+
+        } else {
+            printf(MSGERRR "NOT A VALID COMMAND" MSGNORM "\n");
+            printf(MSGERRR "NOT COMMANDS:" MSGNORM "\n");
+            printf(MSGERRR "    LIST" MSGNORM "\n");
+            printf(MSGERRR "    GET <file name>" MSGNORM "\n");
+            printf(MSGERRR "    PUT <file name>" MSGNORM "\n");
+            printf(MSGERRR "    EXIT" MSGNORM "\n");
+        }
+    }
+
+    printf("\n");
+}
+
+
+/* handles listing the files from the servers */
+int handle_LIST(){
+    printf(MSGSUCC "--Listing Files--" MSGNORM "\n");
+    /* start off getting file parts */
+    char files[FILEPRTS][FILEPRTS];
+    int parts_of_file[FILEPRTS][serverconfigs.ServerNum];
+    int i; int j;
+    for(i = 0; i < FILEPRTS; i++){
+        for(j = 0; j < serverconfigs.ServerNum; j++){
+            parts_of_file[i][j]=0;
+        }
+    }
+
+    int numberFiles=0; // number of files starts off at 0
+    char buff[MAXBUFF];
+    char* bufftok;
+    int serverNum;
+    for(serverNum = 0; serverNum < NUMSERVS; serverNum++){
+        if(send_command_to_server(serverNum, "LIST", buff) < 0){ continue; }
+        bufftok = strtok(buff, "\n");
+
+        while(bufftok != NULL){
+            char fname[FILENAME];
+            int file_piece;
+            int is_newFile = 1;
+
+            sscanf(bufftok, ".%s", fname);
+            file_piece = fname[strlen(fname)-1] - '0';
+            fname[strlen(fname)-2] = '\0';
+
+            for(i=0; i<numberFiles; i++){
+                if(strcmp(fname, files[i])){
+                    parts_of_file[i][file_piece-1]=1;
+                    is_newFile = 0;
+                    break;
+                }
+            }
+
+            if(is_newFile){
+                strcpy(files[numberFiles], fname);
+                parts_of_file[numberFiles][file_piece]=1;
+                ++numberFiles;
+            }
+
+            bufftok = strtok(NULL, "\n");
+
+        }
+
+        for(i=0; i < numberFiles; ++i){
+            char fname[FILENAME];
+            int val=0;
+            for(j=0; j<serverconfigs.ServerNum; ++j){
+                val += (pow(10, j)*parts_of_file[i][j]);
+            }
+
+            strcpy(fname, files[i]);
+
+            if(val < 1111){printf(MSGERRR "Incomplete Name" MSGNORM "\n");}
+            else{ printf(".File Name: %s\n", fname);}
+        }
+
+    }
+
+}
+
+
+/* handles getting the file from the servers */
+int handle_GET(const char* filename){
+    printf(MSGSUCC "--Getting Files--" MSGNORM "\n");
+
+}
+
+
+/* handles putting the file to the servers */
+int handle_PUT(const char* filename){
+    printf(MSGSUCC "--Putting Files--" MSGNORM "\n");
+
+}
+
+
+/* handles exiting program */
+int handle_EXIT(){
+    printf(MSGSUCC "--EXITING PROGRAM--" MSGNORM "\n");
+    return 0;
+}
+
+
+/* connects/sends command to server*/
+int send_command_to_server(int serverNumber, char* argcmd, ...){
+    printf(MSGSUCC "--Sending Command to Server--" MSGNORM "\n");
+    /* stopping here! */
 }
--- JasonLubrano_PA4/server/dfs.c
+++ JasonLubrano_PA4/server/dfs.c
@@ -1,6 +1,9 @@
 /* 
- * client/dfs.c - client sided distributed file sharing system
+ * Jason Lubrano
+ * CSCI4273 - Ha - PA4
+ * server/dfs.c - server side of the distributed file sharing system
  */
+
 
 /*
  * includes
@@ -39,13 +42,17 @@
 #define MSGTERM "\x1b[35m"
 #define MSGWARN "\x1b[33m"
 
+
 /*
  * lengths
  */
-#define MD5_LEN     16      /* max length for md5 */
-#define SHORTBUF    256     /* max text line length */
-#define MAXLINE     8192    /* max text line length */
+#define NUMSERVS    4       /* max number of servers */
+#define SERVPORT    8       /* max server port length */
+#define MAXCONN     32      /* max number of connections */
+#define FILEDIR     128     /* max file directory length */
+#define MAXLINE     256     /* max text line length */
 #define LISTENQ     1024    /* second argument to listen() */
+#define MAXBUFF     8192    /* max text line length */
 
 
 /*
@@ -53,29 +60,107 @@
  */
 #define VERBOSE 1
 
+
 /*
  * function headers
  */
 void welcome_message();
 void closing_message();
+void ret_exit();
+void data_from_config(const char* configFile, const char* serverFileDirectory);
+int socket_connection_routine(const char* port);
+
 
 /*
  * global timeout
  */
 struct timeval timeout;
 
+
+struct UserConfigs {
+	int		UserNum;
+	char	UserUsername[MAXLINE];
+	char	UserPassword[MAXLINE];
+    char    FileDirectory[FILEDIR];
+} userconfigs;
+
+
 /*
  * main
  */
 int main(int argc, char **argv){
     welcome_message();
+    
+    char* serverFileDirectory;
+    char* port;
+    switch (argc)
+    {
+    case 3:
+        serverFileDirectory = argv[1];
+        if(VERBOSE){ printf(MSGNORM "Server Directory: %s" MSGNORM "\n", serverFileDirectory); }
+        port = argv[2];
+        if(VERBOSE){ printf(MSGNORM "Port: %s" MSGNORM "\n", port); }
+        break;
+    
+    default:
+        printf(MSGERRR "Usage 2: %s <Server Directory> <Port>" MSGNORM "\n", argv[0]);
+        ret_exit(0);
+        break;
+    }
+
+    char* configFile = "dfs.conf";
+    data_from_config(configFile, serverFileDirectory);
+    
+    struct sockaddr_in clientAddrIn;
+    int sockfd;
+
+    sockfd = socket_connection_routine(port);
+    int fileDescriptorSet = getdtablesize();
+    if(VERBOSE){ printf("..File Descriptor Set: %d\n", fileDescriptorSet); }
+
+    fd_set activeFileDescriptorSet;
+    FD_ZERO(&activeFileDescriptorSet);
+    FD_SET(sockfd, &activeFileDescriptorSet);
+
+    int isRunning = 1;
+    unsigned int addressLength;
+    fd_set readFileDescriptorSet;
+    while(isRunning){
+        memcpy(&readFileDescriptorSet, &activeFileDescriptorSet, sizeof(readFileDescriptorSet));
+        
+        if(select(fileDescriptorSet, &readFileDescriptorSet, NULL, NULL, NULL) < 0) {
+            printf( MSGERRR "ERROR: On selecting file descriptor" MSGNORM "\n");
+        }
+       
+        if(FD_ISSET(sockfd, &readFileDescriptorSet)){
+            int sockfdConn;
+            addressLength = sizeof(clientAddrIn);
+            sockfdConn = accept(sockfd, (struct sockaddr *)&clientAddrIn, &addressLength);
+
+            if(sockfdConn < 0){ printf(MSGERRR "ERROR: Connection Rejected" MSGNORM "\n"); ret_exit(); }
+            FD_SET(sockfdConn, &activeFileDescriptorSet);
+        }
+
+        int sockcmdds;
+        for(sockcmdds=0; sockcmdds < fileDescriptorSet; ++sockcmdds){
+            if(FD_ISSET(sockcmdds, &readFileDescriptorSet) && sockcmdds != sockfd){
+                /* STOPPING HERE */
+            }
+        }
+    }
+
+
     closing_message();
+    
     return 0;
 }
+
 
 /*
  * function definitons
  */
+
+/* prints welcome message */
 void welcome_message(){
     printf(MSGSUCC "------------------------------------" MSGNORM "\n");
     printf(MSGSUCC "        Started DFS - Server" MSGNORM "\n");
@@ -84,8 +169,127 @@ void welcome_message(){
 }
 
 
+/* prints closing message */
 void closing_message(){
     printf(MSGSUCC "------------------------------------" MSGNORM "\n");
     printf(MSGSUCC "        Closing DFS - Server" MSGNORM "\n");
     printf(MSGSUCC "------------------------------------" MSGNORM "\n");
+}
+
+
+/* exit(0) upon called */
+void ret_exit(){
+    closing_message();
+    exit(0);
+}
+
+
+/* gets data from config file */
+void data_from_config(const char* configFile, const char* serverFileDirectory){
+    if(VERBOSE) { printf(MSGNORM "--Getting Data from Config--" MSGNORM "\n"); }
+    FILE* file = fopen(configFile, "r");
+    if(file == NULL){ printf(MSGERRR ".config file is NULL \nDoes it exist?" MSGNORM "\n"); ret_exit(); }
+    else {
+        printf(MSGSUCC ".Config file opened successfully" MSGNORM "\n");
+        ssize_t read;
+        char line[MAXLINE];
+        size_t len = 0;
+        char* firstinfo;
+        char* rootdirectory;
+        char* username;
+        char* password;
+        char* delim = " ";
+        int usercount = 0;
+        if(VERBOSE){ printf(".Reading through file line by line \n"); }
+        /* I'm a car too, you know! >:( */
+        while (fgets(line, sizeof(line), file))
+        {
+            printf("%s", line);
+            printf("\n");
+
+            firstinfo = strtok(line, delim);
+            if(VERBOSE){ printf("firstinfo: %s \n", firstinfo); }
+            
+            if(strcmp("RootDirectory", firstinfo) == 0) {
+                if(VERBOSE){ printf(MSGSUCC "Match on Root Directory" MSGNORM "\n"); }
+                
+                rootdirectory = strtok(NULL, delim);
+                if(VERBOSE){ printf("Root Directory: %s \n", rootdirectory); }
+
+                char servDir[MAXLINE];
+                sprintf(servDir, "./%s%s/", rootdirectory, serverFileDirectory);
+                if(VERBOSE){ printf("Server Directory: %s \n", servDir); }
+                
+                strcpy(userconfigs.FileDirectory, servDir);
+                printf("User Configs -> Server File Directory: %s \n", userconfigs.FileDirectory);
+
+            } else if(strcmp("Username:", firstinfo) == 0){
+                if(VERBOSE){ printf(MSGSUCC "Match on Username" MSGNORM "\n"); }
+                
+                username = strtok(NULL, delim);
+                if(VERBOSE){ printf("username: %s \n", username); }
+                
+                strcpy(userconfigs.UserUsername, username);
+                printf("User Config -> Username: %s \n", userconfigs.UserUsername);
+
+            } else if(strcmp("Password:", firstinfo) == 0){
+                if(VERBOSE){ printf(MSGSUCC "Match on Password" MSGNORM "\n"); }
+                
+                password = strtok(NULL, delim);
+                if(VERBOSE){ printf("password: %s \n", password); }
+                
+                strcpy(userconfigs.UserPassword, password);
+                printf("User Config -> Password: %s \n", userconfigs.UserPassword);
+
+            } else {
+                printf(MSGERRR "ERROR IN CONFIG PARSING" MSGNORM "\n\n");
+                ret_exit();
+            }
+            ++usercount;
+        }
+
+        userconfigs.UserNum = usercount-2;
+        printf("User Config -> User Count: %d \n", userconfigs.UserNum);
+    }
+}
+
+
+/* TCP routine to connect sockets */
+int socket_connection_routine(const char* port){
+    if(VERBOSE){printf("--Running Socket Connection Routine--\n"); }
+	struct sockaddr_in sockAddrIn;
+	memset(&sockAddrIn, 0, sizeof(sockAddrIn));
+    sockAddrIn.sin_family = AF_INET;
+    sockAddrIn.sin_addr.s_addr = INADDR_ANY;
+    sockAddrIn.sin_port = htons((unsigned short)atoi(port));
+    if(sockAddrIn.sin_port == 0){ printf(MSGERRR "ERROR: UNABLE TO PORT ON: %s" MSGNORM "\n", port); ret_exit(); }
+    else { printf(MSGSUCC "Port on: %s Established" MSGNORM "\n", port); }
+    
+    int sockfd;
+    sockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(sockfd < 0){ printf(MSGERRR "ERROR: UNABLE TO CREATE SOCKET" MSGNORM "\n"); ret_exit(); }
+    else{ printf(MSGSUCC "Socket Created Successfully" MSGNORM "\n"); }
+
+    if(bind(sockfd, (struct sockaddr *)&sockAddrIn, sizeof(sockAddrIn)) < 0){
+        printf(MSGERRR "Error: Unable to bind port on: %s" MSGNORM "\n", port);
+        sockAddrIn.sin_port = htons(0);
+        if(bind(sockfd, (struct sockaddr *)&sockAddrIn, sizeof(sockAddrIn)) < 0){
+            printf(MSGERRR "Error: Unable to bind port on: %s" MSGNORM "\n", port);
+            ret_exit();
+        } else {
+            int socketLength = sizeof(sockAddrIn);
+            if(getsockname(sockfd, (struct sockaddr *)&sockAddrIn, &socketLength) < 0){
+                printf(MSGERRR "Error Getting Socket Name" MSGNORM "\n");
+            }
+            printf("New Server Port on: %d \n", ntohs(sockAddrIn.sin_port));
+        }
+    }
+
+    if(listen(sockfd, MAXCONN) < 0){
+        printf("Unable to listen on port: %s \n", port);
+        ret_exit();
+    }
+
+    printf("Socket: %d connected on port: %d \n", sockfd, ntohs(sockAddrIn.sin_port));
+    return sockfd;
 }
